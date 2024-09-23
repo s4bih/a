8f8f346991161de7ee1fde8d57d551bb6b6e3bbc
msabi <msabihamin@gmail.com>
2024-09-23 21:32:49 +0800

    akhirnya

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-from werkzeug.utils import redirect
+
 
 app = Flask(__name__)
 with open('config.json', 'r') as c:
@@ -31,35 +31,80 @@ class Siswa(db.Model):
     tempattanggallahir = db.Column(db.String(50), nullable=False)
     alamat = db.Column(db.String(100), nullable=False)
     jeniskelamin = db.Column(db.String(50), nullable=False)
-    namaibu = db.Column(db.String(50), nullable=False)
-    namabapak = db.Column(db.String(50), nullable=False)
+    namaibu = db.Column(db.String(50), nullable=True)
+    namabapak = db.Column(db.String(50), nullable=True)
+    def __init__(self,nama,tempattanggallahir,alamat,jeniskelamin,namaibu,namabapak):
+        self.nama = nama
+        self.tempattanggallahir = tempattanggallahir
+        self.alamat = alamat
+        self.jeniskelamin = jeniskelamin
+        self.namaibu = namaibu
+        self.namabapak = namabapak
 
 @app.route("/",methods=['GET'])
 
 def siswa():
 
     s = Siswa.query.all()
-    print(s)
-
     return render_template('crud.html',s=s)
+
+
 
 @app.route("/add",methods=['GET','POST'])
 def add():
-
-    if request.method=='POST':
-        id = request.form.get('id')
+    print("ye",request.method)
+    print("ya",request.form)
+    if request.method == 'POST':
+        print("ye")
         nama = request.form.get('nama')
+
+
         tempattanggallahir = request.form.get('tempattanggallahir')
         alamat = request.form.get('alamat')
         jeniskelamin = request.form.get('jeniskelamin')
-        namaibu = request.form.get('nm.ibu')
-        namabapak = request.form.get('nm.bapak')
-        s = Siswa(id=id,nama=nama,tempattanggallahir=tempattanggallahir,alamat=alamat,jeniskelamin=jeniskelamin,namaibu=namaibu,namabapak=namabapak)
+        namaibu = request.form.get('namaibu')
+        namabapak = request.form.get('namabapak')
+        s = Siswa(nama=nama,tempattanggallahir=tempattanggallahir,alamat=alamat,jeniskelamin=jeniskelamin,namaibu=namaibu,namabapak=namabapak)
         db.session.add(s)
         db.session.commit()
-    return render_template('add.html')
+        print("ye")
+        return redirect(url_for('siswa'))
+    return render_template('add.html',params=params)
+
+@app.route("/update/<string:post_id>", methods=['GET', 'POST'])
+def update(post_id):
+    post = Siswa.query.filter_by(id=post_id).first()
+    if request.method == 'POST':
+        post.nama = request.form.get('nama')
+        post.tempattanggallahir = request.form.get('tempattanggallahir')
+        post.alamat = request.form.get('alamat')
+        post.jeniskelamin = request.form.get('jeniskelamin')
+        post.namaibu = request.form.get('namaibu')
+        post.namabapak = request.form.get('namabapak')
+        db.session.commit()
+        return redirect(url_for('Siswa'))
+
+@app.route("/delete/<string:post_id>", methods=['GET', 'POST'])
+def delete(post_id):
+    post = Siswa.query.filter_by(id=post_id).first()
+    if post is None:
+        return "Post not found", 404
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('siswa'))
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    app.run()
-    debug = True
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True,port=8000)
+
